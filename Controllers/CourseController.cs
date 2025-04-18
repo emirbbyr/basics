27969a4e7e733633d854d3b7b24b46f6353de179
Emirhan Bayır <emirbbyr@gmail.com>
2025-04-18 17:16:28 +0300

    güncelleme1

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -10,6 +10,7 @@ public IActionResult Index()
     kurs.Id=1;
     kurs.Title="Aspnet kursu";
     kurs.Description="Güzel bir kurs";
+    kurs.Image="1.jpg";
     return View(kurs);  //kurs objesini view üzerinde görüntülemek için parantez içine yazdık.
    
 }
@@ -17,9 +18,9 @@ public IActionResult List()
 {
     var kurslar = new List<Course>()
     {
-        new Course () {Id=1, Title ="aspnet kursu", Description="Güzel bir krs"},
-         new Course () {Id=2, Title ="php kursu", Description="Güzel bir krs"},
-          new Course () {Id=3, Title ="djngo kursu", Description="Güzel bir krs"},
+        new Course () {Id=1, Title ="aspnet kursu", Description="Güzel bir krs",Image="1.jpg"},
+         new Course () {Id=2, Title ="php kursu", Description="Güzel bir krs",Image="1.jpg"},
+          new Course () {Id=3, Title ="djngo kursu", Description="Güzel bir krs",Image="1.jpg"},
     };
      return View(kurslar);   
 }
